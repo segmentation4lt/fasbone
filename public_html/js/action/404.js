0e5ae4d5890e580fab36983d53fe93eca5967c24
seg4 okamura <info@seg4.biz>
2025-03-25 11:22:15 +0900

    待ち受け処理のポーリング化と発火点定義をコールバックで呼び出す処理を追加

--- public_html/js/action/404.js
+++ public_html/js/action/404.js
@@ -6,17 +6,39 @@
 //-----------------------------------------------------//
 // Actionに応じたAPIリクエストを実施し、結果をDataオブジェクトに反映させる
 //-----------------------------------------------------//
-async function Execute(){
-    //-----------------------------------------------------//
-    // CRUD処理の場合に必要なtokenはclientinfoは
-    // wasmロード処理時に取得済
-    //-----------------------------------------------------//
-
-
+function Execute(){
     //-----------------------------------------------------//
     // 動作確認
     //-----------------------------------------------------//
     console.log(clientinfo);//クライアント環境情報
-  
-}
+
+    //-----------------------------------------------------//
+    // Render(template出力)処理
+    // 引数:<common|action/テンプレート名>,<出力対象タグのid>,<JSON文字列>
+    //-----------------------------------------------------//
+    //wasm.bonerender("action/template_name","main",clientinfo.page_api.api_data);
+    
+    //描画終了を確認後、待受処理を実施(発火点)
+    const observer = new MutationObserver((mutationsList, observer) => {
+        for (let mutation of mutationsList) {
+            if (mutation.type === "childList") {
+                const targetElement = document.getElementById('main');
+                if (targetElement) {//描画終了確認
+                    observer.disconnect(); // 監視を停止
+                    // イベントを追加
+                    EventAction();
+                }
+            }
+        }
+    });
+    observer.observe(document.body, { childList: true, subtree: true });
+
+};
+
+//-----------------------------------------------------//
+// 待ち受け処理をここに記載
+//-----------------------------------------------------//
+function EventAction(){
+
+};
 
